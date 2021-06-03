ba2006bbeae5de64dd2af86b20288f3d172687fb
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-06-03 12:58:37 -0400

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,47 @@
 
 
-it("does the a test 1", ()=> {
+// it("does the a test 1", ()=> {
+//     console.log("doing a test with it!!!");
+// })
+
+// describe("positive tests", ()=>{
+//     test("does the a test 1", ()=>{
+//         console.log("doing a test");
+//     });
     
-})
+//     test("does the a test 2", ()=>{
+//         console.log("doing a test");
+//     });
+    
+//     test("does the a test 3", ()=>{
+//         console.log("doing a test");
+//     });
+// });
 
-test("does the a test 1", ()=>{
-    console.log("doing a test");
-});
+// describe("negative tests", ()=>{
+//     test("does the a test 1", ()=>{
+//         console.log("doing a test");
+//     });
+    
+//     test("does the a test 2", ()=>{
+//         console.log("doing a test");
+//     });
+    
+//     test("does the a test 3", ()=>{
+//         console.log("doing a test");
+//     });
+// });
 
-test("does the a test 2", ()=>{
-    console.log("doing a test");
-});
-
-test("does the a test 3", ()=>{
-    console.log("doing a test");
-});
+// describe("edge cases", ()=>{
+//     test("does the a test 1", ()=>{
+//         console.log("doing a test");
+//     });
+    
+//     test("does the a test 2", ()=>{
+//         console.log("doing a test");
+//     });
+    
+//     test("does the a test 3", ()=>{
+//         console.log("doing a test");
+//     });
+// });
